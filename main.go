ececb17ab77f7d13e031ab0aa0e55ba92f7559a2
Fizedd <111342647+Fizedd@users.noreply.github.com>
2024-07-05 21:07:22 +0500

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ var intToRomanMap = []struct {
 	value  int
 	symbol string
 }{
-	{1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+	
 	{100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
 	{10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"},
 }
